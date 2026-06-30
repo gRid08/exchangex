57e34150bddc2e793dcb5552762e058c22acff6f
Ashutosh Devanpalli <ashutoshdevanpalli86@gmail.com>
2026-07-01 00:05:38 +0530

    Initial commit

---
+++ src/main/java/com/exchangex/order_matching_engine/OrderMatchingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.exchangex.order_matching_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMatchingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderMatchingEngineApplication.class, args);
+	}
+
+}
